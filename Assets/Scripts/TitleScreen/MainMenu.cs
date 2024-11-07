fcf591bb3b55f8350f35e0d80182959c6768b03b
chris8332558 <chris8332558@gmail.com>
2024-11-07 14:33:01 -0800

    Add simple titlescreen with rotating cude

---
+++ Assets/Scripts/TitleScreen/MainMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour
+{
+    public string NextSceneName;
+    public float rotatePeriod;
+    [SerializeField] Transform cube;
+
+    private void Start()
+    {
+        cube.DORotate(new Vector3(0, 360, 0), rotatePeriod, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(NextSceneName);
+	}
+
+    public void Setting()
+    {
+        Debug.Log("Open Setting Page");
+	}
+
+    public void ExitGame()
+    {
+        Application.Quit();
+	}
+}
