776a4df156c5c16280056615252ffd7c5851c8c2
chris8332558 <chris8332558@gmail.com>
2024-11-08 11:28:20 -0800

    Add button hover behavior

--- Assets/Scripts/TitleScreen/MainMenu.cs
+++ Assets/Scripts/TitleScreen/MainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 
@@ -6,16 +7,33 @@ public class MainMenu : MonoBehaviour
 {
     public string NextSceneName;
     public float rotatePeriod;
+    public Button startButton; 
+    public Button settingButton; 
+    public Button exitButton;
+    public float hoverButtonRatio;
     [SerializeField] Transform cube;
 
+    private Vector3 originalButtonScale;
+    
     private void Start()
     {
-        cube.DORotate(new Vector3(0, 360, 0), rotatePeriod, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        cube.DORotate(new Vector3(360, 360, 0), rotatePeriod, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        originalButtonScale = startButton.transform.localScale;
     }
 
     public void NewGame()
     {
         SceneManager.LoadScene(NextSceneName);
+	}
+
+    public void MouseOnButton(Button aButton)
+    {
+        aButton.transform.DOScale(aButton.transform.localScale * hoverButtonRatio, 0.2f);
+	}
+
+    public void MouseExitButton(Button aButton)
+    {
+        aButton.transform.DOScale(originalButtonScale, 0.2f);
 	}
 
     public void Setting()
