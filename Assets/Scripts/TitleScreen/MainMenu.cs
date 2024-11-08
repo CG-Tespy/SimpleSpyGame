21d88c964bff29cf71fb1d00ea27eddfdffecfdc
chris8332558 <chris8332558@gmail.com>
2024-11-08 14:14:05 -0800

    Add button sliding in

--- Assets/Scripts/TitleScreen/MainMenu.cs
+++ Assets/Scripts/TitleScreen/MainMenu.cs
@@ -7,18 +7,33 @@ public class MainMenu : MonoBehaviour
 {
     public string NextSceneName;
     public float rotatePeriod;
-    public Button startButton; 
-    public Button settingButton; 
-    public Button exitButton;
+    public Transform buttonParent;
+    public Button[] buttons;
     public float hoverButtonRatio;
+    public float buttonMoveDuration;
+    public float buttonMoveDelay;
     [SerializeField] Transform cube;
 
     private Vector3 originalButtonScale;
-    
+    private bool isButtonInteractable;
+
+    private void Awake()
+    {
+        foreach (Button btn in buttons)
+        {
+            btn.enabled = false;
+		}
+    }
+
     private void Start()
     {
         cube.DORotate(new Vector3(360, 360, 0), rotatePeriod, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
-        originalButtonScale = startButton.transform.localScale;
+        buttonParent.DOLocalMoveX(200, buttonMoveDuration).SetEase(Ease.OutBack).SetDelay(buttonMoveDelay).OnComplete(()=>
+        {
+            EnableButtons();
+        });
+
+        originalButtonScale = buttons[0].transform.localScale;
     }
 
     public void NewGame()
@@ -28,11 +43,13 @@ public class MainMenu : MonoBehaviour
 
     public void MouseOnButton(Button aButton)
     {
+        if (!isButtonInteractable) return;
         aButton.transform.DOScale(aButton.transform.localScale * hoverButtonRatio, 0.2f);
 	}
 
     public void MouseExitButton(Button aButton)
     {
+        if (!isButtonInteractable) return;
         aButton.transform.DOScale(originalButtonScale, 0.2f);
 	}
 
@@ -45,4 +62,13 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
 	}
+
+    private void EnableButtons()
+    {
+        isButtonInteractable = true;
+        foreach (Button btn in buttons)
+        {
+            btn.enabled = true;
+        }
+    }
 }
