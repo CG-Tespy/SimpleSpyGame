fbffb749a96641101eb68cca0866126f91a7fac8
CG-Tespy <gbcabreracgt@gmail.com>
2024-11-15 10:32:58 -0500

    Implemented basic doc-retrieval | Groundwork for responses to said retrieval

---
+++ Assets/Scripts/GovernmentDocument.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FightToTheLast
+{
+    public class GovernmentDocument : MonoBehaviour
+    {
+        public virtual void OnCollect()
+        {
+            if (IsRetrieved)
+            {
+                return;
+            }
+
+            IsRetrieved = true;
+
+            // If we decide to add a visual indicator of this being retrieved, we might
+            // want to work with that here
+            AnyRetrieved(this);
+        }
+
+        public virtual bool IsRetrieved { get; protected set; }
+        public static event System.Action<GovernmentDocument> AnyRetrieved = delegate { };
+    }
+}
