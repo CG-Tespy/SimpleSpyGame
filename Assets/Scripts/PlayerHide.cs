00e90f52574dbf1ddcf63ca59b25cd775bf1d145
CG-Tespy <gbcabreracgt@gmail.com>
2024-11-15 18:13:11 -0500

    Implemented detection of hiding spots

---
+++ Assets/Scripts/PlayerHide.cs
@@ -0,0 +1,61 @@
+using CGT.Events;
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FightToTheLast
+{
+    public class PlayerHide : MonoBehaviour
+    {
+        [Tooltip("Helps us know when we're in range of a hiding spot")]
+        [SerializeField] protected Collider _spotDetector;
+        [SerializeField] [Tag] protected string _hidingSpotTag;
+
+        protected virtual void Awake()
+        {
+            if (!_spotDetector.TryGetComponent(out _collisionEvents))
+            {
+                _collisionEvents = _spotDetector.gameObject.AddComponent<CollisionEventNotifier>();
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            _collisionEvents.TriggerEnter += OnTriggerEnterResponse;
+            _collisionEvents.TriggerExit += OnTriggerExitResponse;
+        }
+
+        protected CollisionEventNotifier _collisionEvents;
+
+        protected virtual void OnTriggerEnterResponse(Collider other)
+        {
+            bool touchedHidingSpot = other.gameObject.CompareTag(_hidingSpotTag);
+
+            if (touchedHidingSpot)
+            {
+                _hidingSpotInRange = other.transform;
+                Debug.Log("Touched hiding spot!");
+            }
+        }
+
+        protected Transform _hidingSpotInRange;
+
+        protected virtual void OnTriggerExitResponse(Collider other)
+        {
+            bool exitedHidingSpot = other.transform != null && other.transform == _hidingSpotInRange;
+
+            if (exitedHidingSpot)
+            {
+                Debug.Log($"Exited hiding spot");
+                _hidingSpotInRange = null;
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            _collisionEvents.TriggerEnter -= OnTriggerEnterResponse;
+            _collisionEvents.TriggerExit -= OnTriggerExitResponse;
+        }
+    }
+}
