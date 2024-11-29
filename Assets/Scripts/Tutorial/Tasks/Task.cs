217e91a261b0894bc46c27ed877da459949fa34a
chris8332558 <chris8332558@gmail.com>
2024-11-28 16:43:40 -0800

    revise tutorial hierarchy

--- Assets/Scripts/Tutorial/Tasks/Task.cs
+++ Assets/Scripts/Tutorial/Tasks/Task.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public abstract class Task : MonoBehaviour
 {
     public string taskName;
-    //public TMP_Text text;
-    public Text text;
-    [TextAreaAttribute] public string taskText;
+    [TextArea] public string taskText;
     [HideInInspector] public Tutorial tutorial;
     public abstract bool Done();
 }
--- Assets/Scripts/Tutorial/Tutorial.cs
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
-using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] private List<Task> tasks;
+    [SerializeField] private Text textShower;
     private Task currentTask;
 
     private void Start()
@@ -32,7 +33,7 @@ public class Tutorial : MonoBehaviour
     { 
         currentTask = tasks[0];
         currentTask.gameObject.SetActive(true);
-        currentTask.text.DOText(currentTask.taskText, 1f);
+        textShower.DOText(currentTask.taskText, 1f);
         tasks.RemoveAt(0);
 	}
 
@@ -48,7 +49,8 @@ public class Tutorial : MonoBehaviour
         currentTask.gameObject.SetActive(false);
         currentTask = tasks[0];
         currentTask.gameObject.SetActive(true);
-        currentTask.text.DOText(currentTask.taskText, 1f);
+        textShower.text = ""; // clear the text before showing
+        textShower.DOText(currentTask.taskText, 1f);
         tasks.RemoveAt(0);
 	}
 }
